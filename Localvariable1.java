6e22af714693e0e3cf9584f505d296bb7f99e493
edison2397 <edisonraja794@gmail.com>
2024-08-12 08:13:28 -0600

    Add files via upload
    Local variable task

---
+++ Localvariable1.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Localvariable1 {
+	void assign()
+	{
+		int age=21;
+		String name="Edison";
+		System.out.println("age is:"+age);
+		System.out.println("name is:"+name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Localvariable1 lc=new Localvariable1();
+		lc.assign();
+
+	}
+
+}
