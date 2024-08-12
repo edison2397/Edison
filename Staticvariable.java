7cda93d546494f476c0547821417137d33573520
edison2397 <edisonraja794@gmail.com>
2024-08-12 08:27:39 -0600

    Add files via upload
    Staticvariable

---
+++ Staticvariable.java
@@ -0,0 +1,12 @@
+package corejava;
+
+public class Staticvariable {
+	static String name="Alfred Edison Raja";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my name is:"+Staticvariable.name);
+
+	}
+
+}
