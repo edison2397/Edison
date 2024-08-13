94b08f9fc121d6004e9100a6a20125cfa3e34eb6
edison2397 <edisonraja794@gmail.com>
2024-08-13 08:26:29 -0600

    Add files via upload
    Daily Task in java->Even numbers upto 50 numbers

---
+++ Javaeven.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Javaeven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=50;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("print even numbers upto 50="+i);
+			}
+		}
+
+	}
+
+}
+
+
