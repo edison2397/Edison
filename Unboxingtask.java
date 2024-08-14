02eff77be60e65425955a72c883adf37063f6c7b
edison2397 <edisonraja794@gmail.com>
2024-08-14 18:27:15 +0530

    Add files via upload
    Daily Task in java-->Wrapper class and unboxing type

---
+++ Unboxingtask.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class Unboxingtask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		Integer in=Integer.valueOf(a);//unboxing class 
+		Integer j=a;
+		System.out.println(a+" "+in+ " "+j);
+
+	}
+
+}
---
+++ Wrap.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class Wrap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer intobject=new Integer(68);
+		int i=intobject.intValue();
+		Boolean boobject=new Boolean(true);
+		boolean b=boobject.booleanValue();
+		System.out.println("The Integer Value is="+i);
+		System.out.println("The Boolean Value is="+b);
+		
+		
+
+	}
+
+}
