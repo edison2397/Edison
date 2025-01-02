e2aca4d123bb77ac3ddee16f1aa3ca90a0be86a2
edison2397 <edisonraja794@gmail.com>
2025-01-02 05:57:07 -0700

    Add files via upload

---
+++ Smallest.java
@@ -0,0 +1,23 @@
+package jv;
+
+public class Smallest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c=30;
+		if(a>b || a>c)
+		{
+			System.out.println("a is smaller");
+		}
+		else if(b>a ||b>c )
+		{
+			System.out.println("b is smaller");
+		}
+		else
+		{
+			System.out.println("c is smaller");
+		}
+
+	}
+
+}
