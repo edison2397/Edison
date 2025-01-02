76f4cbed4c5dbaecd7c9703d498943ad40fafc10
edison2397 <edisonraja794@gmail.com>
2025-01-02 05:55:54 -0700

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,23 @@
+package jv;
+
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sub1=10;
+		int sub2=50;
+		int sub3=90;
+		int average;
+		average=sub1+sub2+sub3/3;
+		if(100>average)
+		{
+		System.out.println("You are the best student");
+		}
+		else
+		{
+		System.out.println("Fail");	
+		}
+		}
+		}
+
