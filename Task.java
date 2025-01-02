833ce39077513fe8921742137bf978d48aaf2032
edison2397 <edisonraja794@gmail.com>
2025-01-02 06:01:52 -0700

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,19 @@
+package jv;
+
+public class Task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		if(i%2==0)
+		{
+			System.out.println("Even Number");
+		}
+		else
+		{
+			System.out.println("Odd Number");
+		}
+
+	}
+
+}
