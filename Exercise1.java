e92607b3d0a622a1cab720e277ba40a324cbcb25
edison2397 <edisonraja794@gmail.com>
2025-01-02 05:56:35 -0700

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,19 @@
+package jv;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=6,b=10;
+		if(a>b)
+		{
+			System.out.println("a is lesser than b");
+		}
+		else
+		{
+			System.out.println("a is greater than b");
+		}
+
+	}
+
+}
